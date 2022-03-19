a78d6b7264830a35382ee6a8f7dfe60df08342c2
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2022-03-19 16:31:12 -0400

    Add: Comments to `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,31 @@ struct {{app_code_name}}App {
 impl AppCore for {{app_code_name}}App {
     fn info(&self) -> AppInfo {
         AppInfo {
-            name: "{{app_display_name}}".into(),
-            publisher: "{{app_publisher_name}}".into(),
-            version: Version(0, 1, 0, 0),
+            // Application name to show to the user
+            name: "{{app_display_name}}".into(),            
+
+            // Publisher name to show to the user 
+            publisher: "{{publisher_display_name}}".into(),
+
+            // Application version
+            version: Version(0, 1, 0, 0),                   
         }
     }
 
     fn utils(&mut self) -> &mut AppUtilities {
+        // Get and return a mutable reference to the `AppUtilities`
         self.utils.as_mut()
     }
 
     fn utils_ref(&self) -> &AppUtilities {
+        // Get and return a reference to the `AppUtilities`
         self.utils.as_ref()
     }
 }
 
 impl AppEvents for {{app_code_name}}App {
     fn start(&mut self) -> Result<(), AppError> {
+        // TODO: Initialize the application
         Ok(())
     }
 
@@ -39,21 +47,27 @@ impl AppEvents for {{app_code_name}}App {
 
 impl AppFactory for {{app_code_name}}App {
     fn make_app() -> Box<Self> {
+        // Construct an `{{app_code_name}}App` on the heap
         Box::new(Self {
-            utils: AppUtilities::new()
+            utils: AppUtilities::new(),
+
+            // Any fields you add to `{{app_code_name}}App` 
+            // MUST be initialied here!
         })
     }
 }
 
 impl AppECS for {{app_code_name}}App {
     fn extra_initializers() -> Vec<Box<dyn ECSInitializer>> {
-        // TODO: Add custom `ECSInitializer` structs to the `Vec` below!
+        // TODO: Add custom `ECSInitializer` structs to the `Vec` below
         vec![]
     }
 }
 
-// The `main` function should always look like this!
 fn main() {
+    // Create/initialize an instance of `{{app_code_name}}App` and then run it.
+    // The `entry` function never returns!
     entry::<{{app_code_name}}App>();
-    // Anything after `entry` will not get run!
+
+    // Anything after `entry` will not get executed!
 }
